5bfd70e2cdeaeda64090caa10353dc108efddd3a
何俊 <251273714@qq.com>
2017-11-22 22:24:20 +0800

    博客基本框架

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -11,33 +11,63 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports={
     entry:{
-        'common/main':[srcPath+'/common/main.js']
+        'common/main':[srcPath+'/common/main.js'],
+        'common/admin-lib':['jquery','bootstrap','BOOTSTRAP_CSS'] ,//public/common/admin-lib.js public/common/admin-lib.css
+        'common/lib':['jquery','APP_CSS']
     },
     output:{
         path:__dirname+'/public',
         filename:'[name].js',
-        publicPath:'http://localhost:8080/public',
+        publicPath:'http://localhost:8080/public/',
     },
-    module:{
-        rules:[
-            {
-                test:/\.(png|jpg)$/,
-                //如果图片小于8k  8192byte  就将图片Base64编码成字符串
-                use:'url-loader?limit=8192&context=client&name=/img/[name].[ext]'
+    resolve:{
+        modules:[srcPath,'node_modules'],//指定webpack查找文件目录
+        //取别名，在自己的js里面直接使用这个别名
+        alias: {
+            SRC:srcPath ,
+            BOOTSTRAP_CSS:'bootstrap/dist/css/bootstrap.css',
+            BOOTSTRAP_TABLE_CSS:'bootstrap-table/dist/bootstrap-table.css',
+            APP_CSS: 'SRC/common/app.less'
+        }
+    },
+    module: {
+        rules: [{
+                test: /\.(png|jpg)$/,
+                use: 'url-loader'
             },
             {
-                test:/\.css$/,
-                use:ExtractTextPlugin.extract({
-					fallback: "style-loader",
-					use: "css-loader"
-				})
+                test: /(\.css|\.less)$/,
+                use: [
+                    'style-loader',
+                    'css-loader?sourceMap', //2
+                    'less-loader' /**处理.less后缀的文件 */
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            //先暂时性把这个加载器注释掉
+
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        plugins: ['transform-runtime','syntax-dynamic-import']
+                    }
+                }
             }
-
-
         ]
     },
     plugins:[
-        new CleanWebpackPlugin(['public']),
+        new CleanWebpackPlugin(['public'],{
+            exclude:['ueditor']
+        }),
         //用来独立css文件和路径的
         new ExtractTextPlugin({
             filename: function (getPath) {
@@ -52,7 +82,8 @@ module.exports={
 			jQuery:'jquery'
         }),
         //混淆压缩
-        new webpack.optimize.UglifyJsPlugin()
+        new webpack.optimize.UglifyJsPlugin(),//这个插件不支持混淆es6
+        
     ]
 
 
